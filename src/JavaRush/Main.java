5de6c94b353804c91ce24c3b2ee4d21707190f0e
steinovski <k.p.electr@gmail.com>
2022-07-19 02:39:10 +0300

    sum 100

--- src/JavaRush/Main.java
+++ src/JavaRush/Main.java
@@ -5,24 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-        int i = 0, j = 1;
-
-//            System.out.pr
-        while (i < 10) {
-            j = 0;
-            System.out.print("|||");
-            while (j < 18) {
-                if (i == 0 || i == 9) {
-                    System.out.print("Б");
-                    j++;
-                } else {
-                    System.out.print(" ");
-                    j++;
-                }
-            }
-            System.out.println("Б");
+        int i=1;
+        int sum=0;
+        while (i<=100) {
             i++;
+            if (i%3==0)
+                continue;
+            sum+=i;
         }
+        System.out.println(sum);
     }
-
 }
