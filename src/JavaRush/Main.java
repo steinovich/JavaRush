10f635555640bb293a0057164e3ba7879ec8f3d1
steinovski <k.p.electr@gmail.com>
2022-07-19 00:29:44 +0300

    second commit commit

---
+++ src/JavaRush/Main.java
@@ -0,0 +1,28 @@
+package JavaRush;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int i = 0, j = 1;
+
+//            System.out.pr
+        while (i < 10) {
+            j = 0;
+            System.out.print("Б");
+            while (j < 18) {
+                if (i == 0 || i == 9) {
+                    System.out.print("Б");
+                    j++;
+                } else {
+                    System.out.print(" ");
+                    j++;
+                }
+            }
+            System.out.println("Б");
+            i++;
+        }
+    }
+
+}
