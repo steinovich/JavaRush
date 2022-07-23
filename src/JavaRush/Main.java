48d61934c433da7fd6e25b7f70bab4b3e1d01b70
steinovski <k.p.electr@gmail.com>
2022-07-23 15:17:32 +0300

    last changes but not accept java rush

--- src/JavaRush/Main.java
+++ src/JavaRush/Main.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-        //напишите тут ваш код
         Scanner in = new Scanner(System.in);
-        int max = Integer.MIN_VALUE;
-        if (max%2==1)max++;
-        int maxOut=max;
+        int min = Integer.MAX_VALUE;
         int x;
-
-
         while (in.hasNextInt()) {
             x = in.nextInt();
-            if(x%2==0){
-                if (max < x) max = x;
-            }
+            if (min > x) min = x;
         }
-        if (in.nextLine().length() == 1||max == maxOut) System.out.println(Integer.MIN_VALUE);
-        else System.out.println(max);
+        if (in.nextLine().length() == 1) System.out.println(Integer.MAX_VALUE);
+        else System.out.println(min);
     }
 }
